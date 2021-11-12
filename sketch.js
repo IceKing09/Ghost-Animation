30774e56ced32449b64834abc941215f4d9ea382
IceKing09 <93411523+IceKing09@users.noreply.github.com>
2021-11-11 16:48:22 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function setup() {
+  createCanvas(400,400);
+  createSprite(200,200,50,50)
+}
+background("yellow")
+whenRightArrowPressed()
+  background("red")
+
+function draw() 
+{
+  background(30);
+  drawSprites()
+
+}
+
+
+
+
